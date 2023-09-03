bea43bfd1f32c06669dc37738bf60479179a3791
Storm Ryan Johnson <storm@explore-ai.net>
2023-09-04 00:38:59 +0200

    Update features of model

--- app.py
+++ app.py
@@ -6,19 +6,12 @@ from modelling.helper import *
 
 __version__ = 'v0.0.1'
 BASE_DIR = Path(__file__).resolve(strict=True).parent
-columns = ['minutes', 'goals_scored', 'assists', 'previous_start_value', 'delta_value', 
+columns = ['minutes', 'goals_scored', 'assists', 'previous_start_value', 
+           'delta_value', 'bps', 'selected_by_percent',
             'position_DEF', 'position_FWD', 'position_GK',
-            'position_MID', 'teams_arsenal', 'teams_astonvilla',
-            'teams_bournemouth', 'teams_brentford', 'teams_brighton&hovealbion',
-            'teams_burnley', 'teams_cardiffcity', 'teams_chelsea',
-            'teams_crystalpalace', 'teams_everton', 'teams_fulham',
-            'teams_huddersfieldtown', 'teams_leedsunited', 'teams_leicestercity',
+            'position_MID', 'teams_chelsea',
             'teams_liverpool', 'teams_manchestercity', 'teams_manchesterunited',
-            'teams_newcastleunited', 'teams_norwichcity', 'teams_nottinghamforest',
-            'teams_sheffieldunited', 'teams_southampton', 'teams_stokecity',
-            'teams_swanseacity', 'teams_tottenhamhotspur', 'teams_watford',
-            'teams_westbromwichalbion', 'teams_westhamunited',
-            'teams_wolverhamptonwanderers']
+            'teams_newcastleunited', 'teams_tottenhamhotspur']
 
 app = Flask(__name__)
 
@@ -48,6 +41,8 @@ def predict_api():
     for key in data_dict:
         data_df[key] = [data_dict[key]]
     data_df = data_df.fillna(0)
+    data_df = data_df[columns]
+    print(data_df.columns)
     
     # perform prediction and round to nearest 5 (consistent with real-world FPL pricing)
     prediction = round_to_nearest_5(model.predict(data_df)[0])/10
